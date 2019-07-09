7de3f511f4592b242e4f5bcc1966cebd14de8b2c
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-07-09 19:51:22 +0900

    Method implementation commit

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -12,6 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
     fun changeTextView(view: View) {
-        messageTextView.text = "Hello there!"
+        messageTextView.text = "Hello there! Again!"
     }
 }
